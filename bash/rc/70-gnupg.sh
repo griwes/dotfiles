3ea98f6f7f2c1a4572f2f6b1f20e9ff10538ba9e
Michał 'Griwes' Dominiak <griwes@griwes.info>
2015-12-18 00:35:58 +0100

    Fix GPG somewhat.
    This might seem kinda borken, but I think it's mostly correct now.

--- bash/rc/70-gnupg.sh
+++ bash/rc/70-gnupg.sh
@@ -1,6 +1,12 @@
 # make sure we are always running with a gpg-agent
 # also make sure that all the shells use the same gpg-agent session
-gpg-connect-agent /bye &>/dev/null || gpg-agent --daemon --enable-ssh-support --write-env-file "${HOME}/.gpg-agent-info" &>/dev/null
+if [ -f "${HOME}/.gpg-agent-info" ]; then
+    . "${HOME}/.gpg-agent-info"
+    export GPG_AGENT_INFO
+    export SSH_AUTH_SOCK
+    export SSH_AGENT_PID
+fi
+gpg-connect-agent /bye &>/dev/null || gpg-agent --daemon --enable-ssh-support --write-env-file --use-standard-socket "${HOME}/.gpg-agent-info" &>/dev/null
 if [ -f "${HOME}/.gpg-agent-info" ]; then
     . "${HOME}/.gpg-agent-info"
     export GPG_AGENT_INFO
